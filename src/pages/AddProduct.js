300c164ae7036c877b33643e62a5b8ba7337dfd9
ImaranAnsari <i.ansarimca@gmail.com>
2022-10-21 22:01:23 +0530

    server

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -1,17 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 import AddProductForm from "../Components/Product/AddProductForm";
 
 function AddProduct() {
+    const navigate= useNavigate();
     function addProductHandler(newProduct) {
-        console.log("newProduct", newProduct)
         fetch("http://localhost:3004/product", {
             method: "POST",
             body: JSON.stringify(newProduct),
             headers: {
-                'Content-Type':'application/json'
+                'Content-Type': 'application/json'
 
             }
         })
+            .then(Response => navigate("/",{replace:true}))
+            
+            .catch (err=> console.log(err));
     }
     return <AddProductForm addProductHandler={addProductHandler} />
 }
